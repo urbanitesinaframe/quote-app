9907513b1e5b0bda491838c269ecc930e6c53f59
Jimmy Ng <jimmy_ng@outlook.de>
2023-06-13 23:22:51 +0200

    add explanation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function getQuote() {
-  let quote = fetch("https://dummy-apis.netlify.app/api/quote");
-  let quote2 = quote.then((response) => {
-    console.log(response);
-    return response.json();
-  });
-
-  quote2.then((data) => {
-    document.getElementById("quote").innerText = data.quote;
-    document.getElementById("author").innerText = "-" + data.author;
-  });
+  fetch("https://dummy-apis.netlify.app/api/quote")
+    // .then ist eine fetch/response API >> Was passiert mit den empfangenen Daten?
+    .then((response) => {
+      //.json() ist eine fetch/response API >> gefetchte Daten werden als JS Objects (oder manchmal auch String) umgewandelt
+      return response.json();
+    })
+    .then((data) => {
+      document.getElementById("quote").innerText = data.quote;
+      document.getElementById("author").innerText = "-" + data.author;
+    });
 }
 
 document.querySelector("button").addEventListener("click", getQuote);
